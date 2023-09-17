77f3717876594f11ec30e721e58341825f0d732f
fakeryeoh <yeohmz-wm22@student.tarc.edu.my>
2023-09-18 02:16:39 +0800

    PlayerPrefab
    Attack hit box and player animation update.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,14 +8,20 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
+    public SwordAttack swordAttack;
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,38 +32,66 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //If movement input is not 0, try to move
-        if(movementInput != Vector2.zero)
+        if (canMove)
         {
-            bool success = TryMove(movementInput);
-
-            if (!success)
+            //If movement input is not 0, try to move
+            if (movementInput != Vector2.zero)
             {
-                success = TryMove(new Vector2(movementInput.x, 0));
+                bool success = TryMove(movementInput);
 
-                if (!success)
+                if (!success && movementInput.x > 0)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
+
+                if (!success && movementInput.y > 0)
                 {
                     success = TryMove(new Vector2(0, movementInput.y));
                 }
+
+                animator.SetBool("isMoving", success);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+
+            //Set direction of sprite to movement direction
+            if (movementInput.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (movementInput.x > 0)
+            {
+                spriteRenderer.flipX = false;
             }
         }
     }
 
     private bool TryMove(Vector2 direction)
     {
-        int count = rb.Cast(
-                                   direction,
-                                   movementFilter,
-                                   castCollisions,
-                                   moveSpeed * Time.fixedDeltaTime + collisionOffset);
-
-        if (count == 0)
+        if (direction != Vector2.zero)
         {
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
+            int count = rb.Cast(
+                                       direction,
+                                       movementFilter,
+                                       castCollisions,
+                                       moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
         else
         {
+            //Can't move if there's no direction to move in
             return false;
         }
     }
@@ -65,5 +99,35 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnFire()
+    {
+        animator.SetTrigger("swordAttack");
+    }
+
+    public void SwordAttack()
+    {
+        LockMovement();
+        if(spriteRenderer.flipX == true)
+        {
+            swordAttack.AttackLeft();
+
+        }
+        else
+        {
+            swordAttack.AttackRight();
+        }
+  
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovement()
+    {
+        canMove = true;
     }
 }
